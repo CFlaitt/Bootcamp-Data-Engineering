a8284f16d7be72c0926e35e6a823ce48ab0a3f4d
Cleber Flaitt <cflaitt@gmail.com>
2023-02-12 19:01:36 +0000

    updated ingestao.py

--- A005/ingestao.py
+++ A005/ingestao.py
@@ -21,7 +21,10 @@ class mercadobitcoinApi():
     @abstractmethod
     def _get_endpoint(self, **kwargs) -> str:
         pass
-
+    
+    @on_exception(expo, ratelimit.exception.RateLimitException, max_tries=10)
+    @ratelimit.limits(calls=29, period=30)
+    @on_exception(expo, requests.exceptions.HTTPError, max_tries=10)
     def get_data(self, **kwargs) -> dict:
         endpoint = self._get_endpoint(**kwargs)
         logger.info(f"Getting data from endpoint: {endpoint}")
@@ -62,7 +65,6 @@ class dataTypeNotSupportedForIngestionException(Exception):
         self.message = f"Data type {type(data)} is not supported for ingestion"
         super().__init__(self.message)
 
-
 class dataWriter:
 
     def __init__(self, coin: str, api: str) -> None:        
@@ -90,7 +92,22 @@ class dataIngestor(ABC):
         self.coins = coins
         self.default_start_date = default_start_date
         self.writer = writer
-        self._checkpoint = None
+        self._checkpoint = self._load_checkpoint()
+
+    @property()
+    def _checkpoint_filename(self) -> str:
+        return f"{self.__class__.__name__}.checkpoint"
+
+    def _write_checkpoint(self):
+        with open(self._checkpoint_filename, "w") as f:
+            f.write(f"{self._checkpoint}")
+
+    def _load_checkpoint(self) -> datetime.date:
+        try:
+            with open(self._checkpoint_filename, "r") as f:
+                return datetime.datetime.strptime(f.read(), "%Y-%m-%d").date()
+        except FileNotFoundError:
+            return None
 
     def _get_checkpoint(self):
         if not self._checkpoint:
@@ -100,6 +117,7 @@ class dataIngestor(ABC):
 
     def _updete_checkpoint(self, value):
         self._checkpoint = value
+        self._write_checkpoint()
 
     @abstractmethod
     def ingest(self) -> None:
