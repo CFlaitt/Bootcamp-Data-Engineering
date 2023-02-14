9201625ec5bd4567d5f3fe7b4170d2cbd8360a5b
Cleber Flaitt <cflaitt@gmail.com>
2023-02-14 21:14:37 +0000

    update test

--- A005/tests/test_apis.py
+++ A005/tests/test_apis.py
@@ -1,17 +1,20 @@
 import datetime
-
+import pytest
 from apis import DaySummaryApi
 
-def test_get_endpoint_BTC():
-    date = datetime.date(2021, 6, 21)
-    api = DaySummaryApi(coin="BTC")
-    actual = api._get_endpoint(date=date)
-    expected = "https://www.mercadobitcoin.net/api/BTC/day-summary/2021/6/21"
-    assert actual == expected
 
-def test_get_endpoint_ETH():
-    date = datetime.date(2021, 6, 21)
-    api = DaySummaryApi(coin="ETH")
-    actual = api._get_endpoint(date=date)
-    expected = "https://www.mercadobitcoin.net/api/ETH/day-summary/2021/6/21"
-    assert actual == expected
+class TestDaySummaryApi:
+    
+    @pytest.mark.parametrize(
+        "coin, date, expected",
+        [
+            ("BTC", datetime.date(2021, 6, 21), "https://www.mercadobitcoin.net/api/BTC/day-summary/2021/6/21"),
+            ("ETH", datetime.date(2021, 6, 21), "https://www.mercadobitcoin.net/api/ETH/day-summary/2021/6/21"),
+            ("BTC", datetime.date(2021, 1, 2), "https://www.mercadobitcoin.net/api/BTC/day-summary/2021/1/2")
+        ]
+    )
+
+    def test_get_endpoint(self, coin, date, expected):    
+        api = DaySummaryApi(coin=coin)
+        actual = api._get_endpoint(date=date)
+        assert actual == expected
