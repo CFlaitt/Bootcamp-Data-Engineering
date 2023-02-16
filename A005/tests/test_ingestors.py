988b78a480c640eec2fafa4d322e35325de9cf1f
Cleber Flaitt <cflaitt@gmail.com>
2023-02-16 02:46:42 +0000

    refactor

--- A005/tests/test_ingestors.py
+++ A005/tests/test_ingestors.py
@@ -1,11 +1,11 @@
 import datetime
 import pytest
 from unittest.mock import patch, mock_open
-from ingestors import DataIngestor
-from writers import DataWriter
+from mercado_bitcoin.ingestors import DataIngestor
+from mercado_bitcoin.writers import DataWriter
 
 @pytest.fixture
-@patch("ingestors.DataIngestor.__abstractmethods__", set())
+@patch("mercado_bitcoin.ingestors.DataIngestor.__abstractmethods__", set())
 def data_ingestor_fixture():
     return DataIngestor(
             writer=DataWriter, 
@@ -13,7 +13,7 @@ def data_ingestor_fixture():
             default_start_date=datetime.date(2021, 6, 21)
     )
 
-@patch("ingestors.DataIngestor.__abstractmethods__", set())
+@patch("mercado_bitcoin.ingestors.DataIngestor.__abstractmethods__", set())
 class TestIngestors:
 
     def test_checkpoint_filename(self, data_ingestor_fixture):
@@ -35,20 +35,20 @@ class TestIngestors:
         expected = datetime.date(2021, 6, 25)
         assert actual == expected
 
-    @patch("ingestors.DataIngestor._write_checkpoint", return_value=None)
+    @patch("mercado_bitcoin.ingestors.DataIngestor._write_checkpoint", return_value=None)
     def test_update_checkpoint_checkpoint_updated(self, mock, data_ingestor_fixture):
         data_ingestor_fixture._update_checkpoint(value=datetime.date(2019, 1, 1))
         actual = data_ingestor_fixture._checkpoint
         expected = datetime.date(2019, 1, 1)
         assert actual == expected
 
-    @patch("ingestors.DataIngestor._write_checkpoint", return_value=None)
+    @patch("mercado_bitcoin.ingestors.DataIngestor._write_checkpoint", return_value=None)
     def test_update_checkpoint_checkpoint_written(self, mock, data_ingestor_fixture):
         data_ingestor_fixture._update_checkpoint(value=datetime.date(2019, 1, 1))
         mock.assert_called_once()
 
     @patch("builtins.open", new_callable=mock_open, read_data="2021-06-25")
-    @patch("ingestors.DataIngestor._checkpoint_filename", return_value="foobar.checkpoint")
+    @patch("mercado_bitcoin.ingestors.DataIngestor._checkpoint_filename", return_value="foobar.checkpoint")
     def test_write_checkpoint(self, mock_checkpoint_filename, mock_open_file, data_ingestor_fixture):
         data_ingestor_fixture._write_checkpoint()
         mock_open_file.assert_called_with(mock_checkpoint_filename, 'w')
